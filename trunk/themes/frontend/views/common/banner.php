1207ced41493e195b8c543fc8fecf217b3002a60
ndhungvu@gmail.com <ndhungvu@gmail.com@35f0623a-68e4-37c1-7685-66412870485a>
2014-07-22 17:00:52 +0000

    social

--- trunk/themes/frontend/views/common/banner.php
+++ trunk/themes/frontend/views/common/banner.php
@@ -1,16 +1,16 @@
+<?php if(!empty($arrBanner)) {?>
 <div class="container">
-	<?php if(!empty($arrBanner)) {?>
-	<div class="fullwidthbanner-container">
-	    <div class="fullwidthabnner">
-	        <ul>
-	        	<?php foreach ($arrBanner as $banner) {?>
-	            <li data-transition="3dcurtain-vertical" data-slotamount="10" data-masterspeed="300" data-thumb="<?php echo getImage($banner['image'],1200,450,0)?>">
-	                <img src="<?php echo getImage($banner['image'],1200,450,0)?>" />
-	            </li>
-	            <?php }?>
-	        </ul>
-	        <div class="tp-bannertimer tp-bottom"></div>
-	    </div>
+	<div class="fullwidthbanner-container">	
+		<div class="fullwidthabnner">
+			<ul>
+				<?php foreach ($arrBanner as $banner) {?>
+				<li data-transition="3dcurtain-vertical" data-slotamount="10" data-masterspeed="300" data-thumb="<?php echo getImage($banner['image'],1170,750,0)?>">
+					<img src="<?php echo getImage($banner['image'],1170,750,0)?>" />
+				</li>
+				<?php }?>
+			</ul>
+			<div class="tp-bannertimer tp-bottom"></div>
+		</div>
 	</div>
-	<?php }?>
 </div>
+<?php }?>
